f4efd1a440fb351612fbb329539023f7b2d880ad
JMErickson17 <JMEricksonDev@gmail.com>
2017-10-01 11:07:25 -0400

    Initial Commit

---
+++ TakeFlight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TakeFlight
+//
+//  Created by Justin Erickson on 10/1/17.
+//  Copyright © 2017 Justin Erickson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
