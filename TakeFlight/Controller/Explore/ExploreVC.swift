43ec127dadcb212463835a2c042e0476d010e086
JMErickson17 <JMEricksonDev@gmail.com>
2017-12-17 20:05:12 -0500

    Add UITabBarController that holds Explore, Search and Profile Navigation Controllers

---
+++ TakeFlight/Controller/Explore/ExploreVC.swift
@@ -0,0 +1,32 @@
+//
+//  ExploreVC.swift
+//  TakeFlight
+//
+//  Created by Justin Erickson on 12/17/17.
+//  Copyright © 2017 Justin Erickson. All rights reserved.
+//
+
+import UIKit
+
+class ExploreVC: UIViewController {
+    
+    // MARK: Lifecycle
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+    
+    
+    // MARK: Setup
+    
+    private func setupView() {
+        
+    }
+
+}
---
+++ TakeFlight/Controller/Profile/ProfileVC.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileVC.swift
+//  TakeFlight
+//
+//  Created by Justin Erickson on 12/17/17.
+//  Copyright © 2017 Justin Erickson. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    // MARK: Lifecycle
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+    
+    // MARK: Setup
+    
+    private func setupView() {
+        
+    }
+}
