f1c678690e554e7714ea4563c4dbb8e225a3c04d
JMErickson17 <JMEricksonDev@gmail.com>
2018-01-24 16:55:03 -0500

    Setup MyFlightCardView to show departing and returning flights

--- TakeFlight/View/MyFlightCardView/MyFlightCardView.swift
+++ TakeFlight/View/MyFlightCardView/MyFlightCardView.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 class MyFlightCardView: UIView {
+    
+    // MARK: Types
+    
+    private enum CardState {
+        case departingFlight
+        case returningFlight
+    }
+    
+    // MARK: Properties
 
     @IBOutlet var contentView: UIView!
     @IBOutlet weak var longDescriptionLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var stopCountLabel: UILabel!
     @IBOutlet weak var originLabel: UILabel!
+    @IBOutlet weak var departureTimeLabel: UILabel!
     @IBOutlet weak var destinationLabel: UILabel!
+    @IBOutlet weak var arrivalTimeLabel: UILabel!
+    @IBOutlet weak var airlineLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var airplaneImage: UIImageView!
+    
+    private var flightData: FlightData?
+    private var cardState: CardState = .departingFlight {
+        didSet {
+            configureCard(forState: cardState)
+        }
+    }
+    
+    // MARK: Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,6 +52,8 @@ class MyFlightCardView: UIView {
         setupView()
     }
     
+    // MARK: Setup
+    
     private func setupView() {
         Bundle.main.loadNibNamed("MyFlightCardView", owner: self, options: nil)
         addSubview(contentView)
@@ -34,15 +61,74 @@ class MyFlightCardView: UIView {
         contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         contentView.layer.cornerRadius = 5
         contentView.clipsToBounds = true
+        
+        let switchStateRecognizer = UITapGestureRecognizer(target: self, action: #selector(stateChangeWasTapped))
+        airplaneImage.addGestureRecognizer(switchStateRecognizer)
     }
     
-    func configureCard(with flightData: FlightData) {
-        self.longDescriptionLabel.text = flightData.longDescription
-        self.originLabel.text = flightData.departingFlight.originAirportCode
-        self.destinationLabel.text = flightData.departingFlight.destinationAirportCode
+    // MARK: Configuration
+    
+    private func configureCard(forState state: CardState) {
+        switch state {
+        case .departingFlight:
+            UIView.animate(withDuration: 0.5, animations: {
+                self.configureCardForDepartingFlight()
+                self.airplaneImage.transform = CGAffineTransform(rotationAngle: CGFloat.pi - 3.12159)
+            })
+        case .returningFlight:
+            UIView.animate(withDuration: 0.5, animations: {
+                self.configureCardForReturningFlight()
+                self.airplaneImage.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            })
+        }
     }
-
+    
+    private func configureCardForDepartingFlight() {
+        guard let flightData = flightData else { return }
+        self.longDescriptionLabel.text = flightData.longDescription
+        self.durationLabel.text = makeDurationString(withDuration: flightData.departingFlight.duration)
+        self.stopCountLabel.attributedText = makeStopCountString(with: flightData.departingFlight.stopCount)
+        self.originLabel.text = flightData.departingFlight.originAirportCode
+        self.departureTimeLabel.attributedText = makeTimeString(with: flightData.departingFlight.departureTime)
+        self.destinationLabel.text = flightData.departingFlight.destinationAirportCode
+        self.arrivalTimeLabel.attributedText = makeTimeString(with: flightData.departingFlight.arrivalTime)
+        self.airlineLabel.text = flightData.departingFlight.carrier.name
+        self.priceLabel.text = makePriceString(withPrice: flightData.saleTotal)
+    }
+    
+    private func configureCardForReturningFlight() {
+        guard let flightData = flightData else { return }
+        guard let returningFlight = flightData.returningFlight else { return }
+        
+        self.longDescriptionLabel.text = flightData.longDescription
+        self.durationLabel.text = makeDurationString(withDuration: returningFlight.duration)
+        self.stopCountLabel.attributedText = makeStopCountString(with: returningFlight.stopCount)
+        self.originLabel.text = returningFlight.originAirportCode
+        self.departureTimeLabel.attributedText = makeTimeString(with: returningFlight.departureTime)
+        self.destinationLabel.text = returningFlight.destinationAirportCode
+        self.arrivalTimeLabel.attributedText = makeTimeString(with: returningFlight.arrivalTime)
+        self.airlineLabel.text = returningFlight.carrier.name
+        self.priceLabel.text = makePriceString(withPrice: flightData.saleTotal)
+    }
+    
+    @objc private func stateChangeWasTapped() {
+        guard let flightData = flightData else { return }
+        if cardState == .departingFlight && flightData.isRoundTrip {
+            self.cardState = .returningFlight
+        } else if cardState == .returningFlight {
+            self.cardState = .departingFlight
+        }
+    }
+    
+    // MARK: Public API
+    
+    func configureCard(with flightData: FlightData) {
+        self.flightData = flightData
+        configureCardForDepartingFlight()
+    }
 }
+
+// MARK:- MyFlightCardView+containingCell(isSelected: )
 
 extension MyFlightCardView {
     func containingCell(isSelected: Bool) {
@@ -51,5 +137,45 @@ extension MyFlightCardView {
         } else {
             contentView.backgroundColor = UIColor.white
         }
+    }
+}
+
+// TODO: Make FlightDataFormatter
+
+extension MyFlightCardView {
+    func makePriceString(withPrice price: Double) -> String {
+        let price = price as NSNumber
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.roundingMode = .up
+        formatter.roundingIncrement = 1
+        return formatter.string(from: price) ?? ""
+    }
+    
+    func makeTimeString(with dateAndTime: DateAndTimeZone) -> NSAttributedString {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "h:mm a"
+        
+        let departureString = dateAndTime.toLocalDateAndTimeString(withFormatter: formatter)
+        return NSAttributedString(string: departureString)
+    }
+    
+    func makeDurationString(withDuration duration: Int) -> String {
+        let hours = duration / 60
+        let minutes = duration % 60
+        return "\(hours)h \(minutes)m"
+    }
+    
+    func makeStopCountString(with stops: Int) -> NSAttributedString {
+        var string: String
+        var attributes = [NSAttributedStringKey: Any]()
+        if stops == 0 {
+            string = "Non-Stop"
+            attributes[NSAttributedStringKey.foregroundColor] = UIColor.stopCountGreen
+        } else {
+            attributes[NSAttributedStringKey.foregroundColor] = UIColor.stopCountRed
+            string = "\(stops) \((stops == 1 ? "stop" : "stops"))"
+        }
+        return NSAttributedString(string: string, attributes: attributes)
     }
 }
