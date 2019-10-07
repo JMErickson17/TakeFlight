11174aa8003ae1bdfa6a54faf1de19ae17a23b12
Justin Erickson <JMEricksonDev@gmail.com>
2019-10-07 17:48:27 -0400

    Update Appfile.swift
    Remove tokens

--- fastlane/Appfile.swift
+++ fastlane/Appfile.swift
@@ -1,8 +1,8 @@
 var appIdentifier: String { return "com.JustinErickson.TakeFlight" } // The bundle identifier of your app
 var appleID: String { return "JMEricksonDev@gmail.com" } // Your Apple email address
 
-var itcTeam: String? { return "118432590" } // iTunes Connect Team ID
-var teamID: String { return "LCHZAHJK5X" } // Apple Developer Portal Team ID
+var itcTeam: String? { return "" } // iTunes Connect Team ID
+var teamID: String { return "" } // Apple Developer Portal Team ID
 
 
 // For more information about the Appfile, see:
